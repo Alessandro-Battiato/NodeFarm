9777df418f343d8c8e6f1436a6af04f7e7d8f09e
alessandro <alessandroDev@outlook.it>
2026-01-24 16:45:49 +0100

    feat: use non blocking asynchronous methods

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+// Blocking, synchronous way
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 
-const textOutput = `This is what we know about the avocado: ${textInput}. \nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOutput);
+// const textOutput = `This is what we know about the avocado: ${textInput}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOutput);
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      fs.writeFile(
+        ".txt/final.txt",
+        `${data2}\n${data3}`,
+        "utf-8",
+        (err) => {},
+      );
+    });
+  });
+});
