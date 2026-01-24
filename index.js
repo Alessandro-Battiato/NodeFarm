bcd85125b7e52e642bac0c8ee44a779fcf35f6b0
alessandro <alessandroDev@outlook.it>
2026-01-24 16:15:06 +0100

    feat: read and write files using node.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+const textOutput = `This is what we know about the avocado: ${textInput}. \nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOutput);
